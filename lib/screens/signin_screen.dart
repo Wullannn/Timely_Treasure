da1246dceb4354dc063bb76af73d80442e6bfd83
Komariah Wulandari <komariahwulandari_2226240119@mhs.mdp.ac.id>
2024-12-13 12:27:30 +0700

    update signin_screen
    membuat fitur no.telpon/username/email dan password serta login

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -36,6 +36,61 @@ class SigninScreen extends StatelessWidget {
                     ),
                   ],
                 ),
+                SizedBox(height: 30),
+                // Form Login
+                Card(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  elevation: 5,
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        // Input Email
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'No. Handphone/Username/Email',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.person),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        // Input Password
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.lock),
+                          ),
+                          obscureText: true,
+                        ),
+                        SizedBox(height: 15),
+                        // Tombol Login
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Logika login di sini
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blueGrey,
+                              padding: EdgeInsets.symmetric(vertical: 15),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Text(
+                              'LOGIN',
+                              style: TextStyle(fontSize: 16, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
