bb2014f63ca43ff5d057de90a56e9e88cb29071e
mseptiawan <syiffamuqaddimah_2226240143@mhs.mdp.ac.id>
2024-12-13 13:34:48 +0700

    final syiffa

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jamm/screens/signin_screen.dart';
+import 'package:jamm/screens/checkout_screen.dart';
+import 'package:jamm/screens/favorit_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,12 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:'Timle Treasure',
+      title: 'Timle Treasure',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.brown,
       ),
-      home: SigninScreen(),
+      home: CheckoutScreen(),
+      // home: FavoritScreen(),
+      // home: SigninScreen(),
     );
   }
 }
